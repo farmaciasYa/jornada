6bec038bcf0383c0210d4f171da7601b99a07817
Jonathan Urdaneta <johnmub98@gmail.com>
2023-06-14 16:21:43 -0400

    Change: Modificar el nombre de milenium a millennium
    Fix: Arreglar fecha de la sede de de candido

---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- Rredirigimos a la pagina de jornada -->
+<?php header('Location: http://jornada_site.test/jornada/');
+die();
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./jornada/*.{html,js,php}'],
+  theme: {
+    extend: {},
+    screens: {
+      xs: '400px',
+      sm: '520px',
+      md: '768px',
+      desktop: '992px',
+    },
+    colors: {
+      gris: '#777',
+      blanco: '#fff',
+      negro: '#1a1a1a',
+      negroClaro: '#363636',
+      celesteTransparente: '#95D4EA',
+      celesteClaro: '#00B4DA',
+      celeste: '#0073B0',
+      naranja: '#FF5800',
+      naranjaClaro: '#FF8A44',
+      naranjaTransparente: '#FFBD94',
+      transparent: 'transparent',
+    },
+  },
+  plugins: [],
+};
